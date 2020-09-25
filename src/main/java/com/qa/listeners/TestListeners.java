8a6694e07b7d3a4f3067ad85fe9c22ebada8b68f
KashifAli <kashifali0321@hotmail.com>
2020-09-25 16:37:18 +0500

    added code to attach screenshot

--- src/main/java/com/qa/listeners/TestListeners.java
+++ src/main/java/com/qa/listeners/TestListeners.java
@@ -6,6 +6,7 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -34,10 +35,12 @@ public class TestListeners implements ITestListener {
                 + "_" + resultParams.get("platformVersion") + "_" + resultParams.get("deviceName")
                 + File.separator + utils.getDateTime() + File.separator + result.getTestClass().getRealClass().getSimpleName()
                 + File.separator + result.getName() + ".png";
-
+        String completeImagePath = System.getProperty("user.dir") + File.separator + imagePath;
         File file = objBaseTest.getDriver().getScreenshotAs(OutputType.FILE);
         try {
             FileUtils.copyFile(file, new File(imagePath));
+            Reporter.log("This is sample Screenshot");
+            Reporter.log("<a href='" + completeImagePath + "'> <image src='" + completeImagePath + "' height='100' width='100'/></a>");
         } catch (IOException e) {
             e.printStackTrace();
         }
